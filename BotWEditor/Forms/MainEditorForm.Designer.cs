d4ed997aed2f0eba60a1f9f046f475ca7b7ad851
Matt Stevens <matt@handsomematt.co.uk>
2017-04-22 03:13:23 +0100

    Simple Tools option to decompress Yaz0 files.

--- BotWEditor/Forms/MainEditorForm.Designer.cs
+++ BotWEditor/Forms/MainEditorForm.Designer.cs
@@ -30,23 +30,28 @@
         {
             this.editorMainMenuStrip = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.documentationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.issueTrackerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.decompressYaz0ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editorMainMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // editorMainMenuStrip
             // 
+            this.editorMainMenuStrip.ImageScalingSize = new System.Drawing.Size(28, 28);
             this.editorMainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem,
             this.helpToolStripMenuItem});
             this.editorMainMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.editorMainMenuStrip.Name = "editorMainMenuStrip";
-            this.editorMainMenuStrip.Size = new System.Drawing.Size(1264, 24);
+            this.editorMainMenuStrip.Padding = new System.Windows.Forms.Padding(11, 4, 0, 4);
+            this.editorMainMenuStrip.Size = new System.Drawing.Size(2317, 42);
             this.editorMainMenuStrip.TabIndex = 0;
             // 
             // fileToolStripMenuItem
@@ -54,8 +59,15 @@
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(56, 34);
             this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(137, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // helpToolStripMenuItem
             // 
@@ -65,49 +77,58 @@
             this.toolStripSeparator1,
             this.aboutToolStripMenuItem});
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(68, 34);
             this.helpToolStripMenuItem.Text = "Help";
             // 
             // documentationToolStripMenuItem
             // 
             this.documentationToolStripMenuItem.Name = "documentationToolStripMenuItem";
-            this.documentationToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.documentationToolStripMenuItem.Size = new System.Drawing.Size(248, 34);
             this.documentationToolStripMenuItem.Text = "Documentation";
             this.documentationToolStripMenuItem.Click += new System.EventHandler(this.documentationToolStripMenuItem_Click);
             // 
             // issueTrackerToolStripMenuItem
             // 
             this.issueTrackerToolStripMenuItem.Name = "issueTrackerToolStripMenuItem";
-            this.issueTrackerToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.issueTrackerToolStripMenuItem.Size = new System.Drawing.Size(248, 34);
             this.issueTrackerToolStripMenuItem.Text = "Issue Tracker";
             this.issueTrackerToolStripMenuItem.Click += new System.EventHandler(this.issueTrackerToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(154, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(245, 6);
             // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(248, 34);
             this.aboutToolStripMenuItem.Text = "About...";
             this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
-            // exitToolStripMenuItem
+            // toolsToolStripMenuItem
             // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.decompressYaz0ToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(72, 34);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // decompressYaz0ToolStripMenuItem
+            // 
+            this.decompressYaz0ToolStripMenuItem.Name = "decompressYaz0ToolStripMenuItem";
+            this.decompressYaz0ToolStripMenuItem.Size = new System.Drawing.Size(281, 34);
+            this.decompressYaz0ToolStripMenuItem.Text = "Decompress Yaz0...";
+            this.decompressYaz0ToolStripMenuItem.Click += new System.EventHandler(this.decompressYaz0ToolStripMenuItem_Click);
             // 
             // MainEditorForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.ClientSize = new System.Drawing.Size(2317, 1257);
             this.Controls.Add(this.editorMainMenuStrip);
             this.MainMenuStrip = this.editorMainMenuStrip;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "MainEditorForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BotW Editor";
@@ -128,5 +149,7 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem decompressYaz0ToolStripMenuItem;
     }
 }
--- BotWEditor/Forms/MainEditorForm.cs
+++ BotWEditor/Forms/MainEditorForm.cs
@@ -1,6 +1,8 @@
 ﻿using BotWEditor.Forms.Dialogs;
+using BotWLib.Formats;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
 
 namespace BotWEditor.Forms
@@ -31,6 +33,43 @@ namespace BotWEditor.Forms
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void decompressYaz0ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var ofd = new OpenFileDialog())
+                {
+                    if (ofd.ShowDialog(this) != DialogResult.OK)
+                        return;
+
+                    using (var stream = ofd.OpenFile())
+                    using (var outputStream = new MemoryStream())
+                    {
+                        Yaz0.Decompress(stream, outputStream);
+
+                        using (var sfd = new SaveFileDialog())
+                        {
+                            sfd.FileName = ofd.FileName;
+
+                            if (sfd.ShowDialog(this) != DialogResult.OK)
+                                return;
+
+                            using (var outputFile = sfd.OpenFile())
+                            {
+                                outputStream.Seek(0, SeekOrigin.Begin);
+                                outputStream.CopyTo(outputFile);
+                            }
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
